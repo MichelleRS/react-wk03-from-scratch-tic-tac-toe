88d80ff553077c63e0971550c640f4dc854d6ac5
Michelle Ross <miross629@gmail.com>
2023-01-26 12:39:30 -0800

    Start styling board and boxes

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import boardData from '../../board-data.js';
 import Box from '../Box/Box.js';
+import './Board.css';
 
 export default function Board() {
   return (
-    <div>
+    <div className="board">
       {boardData.map((data) => (
         <Box key={data.space} />
       ))}
-      ;
     </div>
   );
 }
