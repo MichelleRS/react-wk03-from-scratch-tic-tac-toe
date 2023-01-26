277dc13b29b21ec77dcda039751cc955e2fce0a8
Michelle Ross <miross629@gmail.com>
2023-01-26 10:42:40 -0800

    Begin GameContext and wrap App with GameProvider

---
+++ src/context/GameContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [board, setBoard] = useState(boardData);
+
+  return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+  return context;
+};
+
+export { GameProvider, useGameContext };
