7623a77ea2ffb637165c7c8959bd04c7fe03a8cf
Michelle Ross <miross629@gmail.com>
2023-01-26 12:31:02 -0800

    Display nine boxes in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Board from './Components/Board/Board.js';
 
 function App() {
   return (
     <div className="App">
       {/* header */}
       {/* board */}
+      <Board />
     </div>
   );
 }
--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import boardData from '../../board-data.js';
+import Box from '../Box/Box.js';
 
+export default function Board() {
+  return (
+    <div>
+      {boardData.map((data) => (
+        <Box key={data.space} />
+      ))}
+      ;
+    </div>
+  );
+}
