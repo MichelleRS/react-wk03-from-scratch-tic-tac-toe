a68ecbd18377d956fd80bd9915d4b0182d67a35f
Michelle Ross <miross629@gmail.com>
2023-01-27 18:02:04 -0800

    Add reset button to Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,12 +2,30 @@ import React from 'react';
 import { useGameContext } from '../../context/GameContext.js';
 
 export default function Header() {
-  const { gameMessage } = useGameContext();
+  const { gameMessage, setBoard, setCurrentPlayer, setActive, setGameMessage } = useGameContext();
+
+  function resetGame() {
+    setActive(true);
+    setBoard([
+      { space: 0, content: '' },
+      { space: 1, content: '' },
+      { space: 2, content: '' },
+      { space: 3, content: '' },
+      { space: 4, content: '' },
+      { space: 5, content: '' },
+      { space: 6, content: '' },
+      { space: 7, content: '' },
+      { space: 8, content: '' },
+    ]);
+    setCurrentPlayer('X');
+    setGameMessage('');
+  }
 
   return (
     <header>
       <h1>Tic-Tac-Toe</h1>
       <p>{gameMessage}</p>
+      <button onClick={resetGame}>New Game</button>
     </header>
   );
 }
